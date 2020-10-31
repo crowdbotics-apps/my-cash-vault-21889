3b8ed405d45f61a4f62e2c1637c2a8171c04047f
Crowdbotics <team@crowdbotics.com>
2020-10-31 02:51:39 +0000

    add package blueprint-react-native-blank-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'BlankScreen4165538', human_name: 'Blank Screen 4', access_route: 'BlankScreen4165538'},
 { name: 'EmailAuth2158262', human_name: 'Email Auth 2', access_route: 'EmailAuth2158262', icon: 'envelope-o'},
 { name: 'Settings3158261', human_name: 'Settings 3', access_route: 'Settings3158261'},
 { name: 'SignIn24158260', human_name: 'Sign In 2 4', access_route: 'SignIn24158260'},
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,6 +5,7 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
+import BlankScreen4165538Navigator from '../features/BlankScreen4165538/navigator';
 import EmailAuth2158262Navigator from '../features/EmailAuth2158262/navigator';
 import Settings3158261Navigator from '../features/Settings3158261/navigator';
 import SignIn24158260Navigator from '../features/SignIn24158260/navigator';
@@ -17,6 +18,7 @@ import SignIn25158259Navigator from '../features/SignIn25158259/navigator';
 const AppNavigator = {
 
     //@BlueprintNavigationInsertion
+BlankScreen4165538: { screen: BlankScreen4165538Navigator },
 EmailAuth2158262: { screen: EmailAuth2158262Navigator },
 Settings3158261: { screen: Settings3158261Navigator },
 SignIn24158260: { screen: SignIn24158260Navigator },
